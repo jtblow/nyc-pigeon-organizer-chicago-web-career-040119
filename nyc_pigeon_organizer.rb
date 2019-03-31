a3329e924153f6bd3db892113cc3fcbe48db645a
Jake Bartlow <jtbartlow@gmail.com>
2019-03-31 19:48:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,15 +6,15 @@ def nyc_pigeon_organizer(data)
     if !final_hash.has_key?(name)
       final_hash[name] = {}
     end
-   if !final_hash[name].has_key?(property)
-          new_hash[name][property] = []
+   if !final_hash[name].has_key?(attribute)
+          new_hash[name][attribute] = []
         end
 
-        if !new_hash[name][property].include?(attribute)
-          new_hash[name][property] << attribute.to_s
+        if !new_hash[name][attribute].include?(details)
+          new_hash[name][attribute] << details.to_s
         end
       end
     end
   end
-  new_hash
+  final_hash
 end
