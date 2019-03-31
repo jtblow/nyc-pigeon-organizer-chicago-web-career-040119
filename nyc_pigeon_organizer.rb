ed5520a103262ffa4dca6d3c56b0163250adf3aa
Jake Bartlow <jtbartlow@gmail.com>
2019-03-31 19:26:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(pigeon_data)
   new_hash = {}
   pigeon_data.each do |attributes, names|
     if new_hash.has_value?(names)
-      new_hash[names] << names
+      new_hash[names][:color] << color
       
     else
       new_hash.names[:color] = [color]
